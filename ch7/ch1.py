10c1b110a77d3011ee7bf4ba384b44be9c5d2de8
nageena88 <ram.nageena88@hotmail.com.com>
2024-08-04 11:53:55 +0530

    added new file of set and if

---
+++ ch7/ch1.py
@@ -0,0 +1,26 @@
+"""decision-making"""
+
+# if
+# if else
+# ladder if else
+# nested if else
+
+# even = []
+# odd = []
+# for x in range(1, 11):
+#     if x % 2:
+#         odd.append(x)
+#     # else:
+#     #     even.append(x)
+#
+# print(even)
+# print(odd)
+
+
+x = 0
+if x < 0:
+    print(x)
+    print("-ve")
+else:
+    print(x)
+    print("+ve")
